fd4dfc5860d31a418cd1819f4b959535dec25997
indiatechtimes <indiatechtimes@gmail.com>
2024-01-02 10:54:35 -0800

    upload new code

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 This is my first Repos. Here we practice. 
 <br>
 This is Mohammad Shadab Khan and I am the onner of this.
-
+<br>
+Author- Mohammad Shadab Khan.
---
+++ findsum.java
@@ -0,0 +1,10 @@
+class find{
+    public static void main(String[] args) {
+        int sum=0;
+        int n=3;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+} 
