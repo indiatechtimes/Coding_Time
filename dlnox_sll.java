0daa734ca0c89f745becdfd5d2fb82edda5b7b37
indiatechtimes <indiatechtimes@gmail.com>
2024-01-04 01:21:44 -0800

    Delete Node At Position X

---
+++ dlnox_sll.java
@@ -0,0 +1,17 @@
+public class dlnox_sll{
+    private ListNode head;
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+        private ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+
+
+    }
+
+
+    
+}
