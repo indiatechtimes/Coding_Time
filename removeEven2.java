cf77f7fb0dca9d753559db0a9cbba444bd151bfa
indiatechtimes <indiatechtimes@gmail.com>
2024-01-04 19:47:36 -0800

    Remove Even Integer from Array

---
+++ removeEven2.java
@@ -0,0 +1,26 @@
+public class removeEven2 {
+    public void re(){
+        int myarray[]=new int[10];
+        myarray[0]=1;
+        myarray[1]=2;
+        myarray[2]=3;
+        myarray[3]=4;
+        myarray[4]=5;
+        myarray[5]=6;
+        myarray[6]=7;
+        myarray[7]=8;
+        myarray[8]=9;
+        myarray[9]=10;
+
+        int l=myarray.length;
+        System.out.println("Length of Array= "+l);
+        System.out.println("Initial Array is below ");
+        for(int i=0;i<l;i++){
+            System.out.print(myarray[i]);
+        }
+        for(int i=0;i<l;i++){
+            
+        }
+
+    }
+}
