cbf05fe920f6fc4a7e10310482f16982f3120587
indiatechtimes <indiatechtimes@gmail.com>
2024-03-18 22:45:55 -0700

    updating

--- DSA/Add_x_no.java
+++ DSA/Add_x_no.java
@@ -5,30 +5,30 @@ import java.util.*;
 public class Add_x_no {
     int sum;
 
-    public void add(){
+    public void add(){//  making function 
         
 
         System.out.println("enter how many number you want to add");
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        int[] myarray =new int [a];
-        for(int i=0;i<a;i++){
-            System.out.println("Enter number");
-            myarray[i]=sc.nextInt();
+        int[] myarray =new int [a];//making array 
+        for(int i=0;i<a;i++){// loop for inserting the items in array
+            System.out.println("Enter next number");// printing massage to reenter the no
+            myarray[i]=sc.nextInt();// inserting element in array
 
         }
 
         
-        for(int i=0;i<a;i++){
-            int s=myarray[i];
-            sum=sum+s;
+        for(int i=0;i<a;i++){// loop for adding all elements(no) one by one 
+            int s=myarray[i];// putting one by one elements in variable s
+            sum=sum+s;// going to add 
         }
         
-        System.out.println("sum of no="+sum);
+        System.out.println("sum of no="+sum);// printing the sum
 
     }
-    public static void main(String[] args) {
-        Add_x_no fiNo=new Add_x_no();
-        fiNo.add();
+    public static void main(String[] args) {// main function
+        Add_x_no fiNo=new Add_x_no(); // making the object of function
+        fiNo.add();// calling of that function
     }
 }
